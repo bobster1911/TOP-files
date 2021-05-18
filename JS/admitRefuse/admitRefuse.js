674bb54b6a45e9b8222033bb04ab892e7c48e297
Rob <47785553+bobster1911@users.noreply.github.com>
2021-05-18 11:12:05 -0700

    started caesar, updated admitRefuse

--- JS/admitRefuse/admitRefuse.js
+++ JS/admitRefuse/admitRefuse.js
@@ -7,16 +7,10 @@ refused.textContent = 'Refuse: ';
 
 for (let i = 0; i < people.length; i++) {
     if (people[i] == 'Phil' || people[i] == 'Lola') {
-        if (i == (people.length - 1)) {
-            refused.textContent += (people[i] + '. ');
-        } else {
             refused.textContent += (people[i] + ', ');
-        }
-    } else {
-        if (i == (people.length - 1)) {
-            admitted.textContent += (people[i] + '. ');
         } else {
             admitted.textContent += (people[i] + ', ');
         }
-    }     
-} // end
+    } // end
+
+
