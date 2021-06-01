de407454c8d26141e571a04e1ef27a904b1b782a
RobPIE <robclarke93@yahoo.co.uk>
2021-06-01 11:41:08 -0700

    practicing DOMs

---
+++ JS/practiceDOMs/practiceDOM.js
@@ -0,0 +1,16 @@
+const div1 = document.createElement('div');
+div1.setAttribute('id', 'div1');
+div1.style.cssText = 'border-style: solid; border-color: coral; background-color: pink';
+document.body.appendChild(div1);
+
+const header1 = document.createElement('h1');
+header1.createElement('h1');
+header1.style.cssText = 'color: black';
+
+const para1 = document.createElement('p');
+para1.setAttribute('id', 'para1');
+para1.style.cssText = 'color: black';
+
+
+
+
