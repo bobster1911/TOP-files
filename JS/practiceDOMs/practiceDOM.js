7d648ab0ed42824f88f63f93bf6f9b7a74611c8a
Rob Clarke <robclarke93@yahoo.co.uk>
2021-06-05 07:08:20 -0700

    worked through js drumkit tutorial

--- JS/practiceDOMs/practiceDOM.js
+++ JS/practiceDOMs/practiceDOM.js
@@ -6,13 +6,21 @@ const header1 = document.createElement('h1');
 header1.setAttribute('id', 'h1');
 header1.textContent = 'I\'m an h1'
 header1.style.cssText = 'color: black';
-document.getElementById('div1').appendChild(header1);
+div1.appendChild(header1);
 
 const para1 = document.createElement('p');
 para1.setAttribute('id', 'para1');
 para1.textContent = 'I\'m a paragraph.';
 para1.style.cssText = 'color: black';
-document.getElementById('div1').appendChild(para1);
+div1.appendChild(para1);
 
 document.body.appendChild(div1);
 
+const btn1 = document.createElement('button');
+btn1.setAttribute('id', 'btn1');
+btn1.textContent = 'Click me!';
+btn1.addEventListener('click', function (e) {
+    e.target.style.background = 'blue';
+});
+
+div1.appendChild(btn1);
