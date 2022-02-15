b395500d3a8b0303c2bc1578327c7a7b0719fa31
Rob <47785553+bobster1911@users.noreply.github.com>
2022-02-15 09:55:18 -0800

    added files and folders to start library

---
+++ JS/ObjectPractice/library.js
@@ -0,0 +1,11 @@
+let myLibrary = [];
+
+function Book(title, pages, read) {
+    this.title = title;
+    this.numberOfPages = pages;
+    this.readYet = read;
+}
+
+function addBookToLibrary() {
+    // do stuff here.
+}
