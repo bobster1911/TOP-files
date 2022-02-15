b6d58154a6deeb77d65eca84516754875bfd2d95
Rob <47785553+bobster1911@users.noreply.github.com>
2022-02-15 15:52:12 -0800

    added some more to library app

--- JS/ObjectPractice/library.js
+++ JS/ObjectPractice/library.js
@@ -7,5 +7,26 @@ function Book(title, pages, read) {
 }
 
 function addBookToLibrary() {
-    // do stuff here.
+    
+    //ask for book information
+    let title = prompt('Title of book: ');
+    let pages = prompt('Number of pages: ');
+    let read = prompt('Read or Unread: ');
+
+    //calculate how to name new addition
+    let numBooks = myLibrary.length;
+    let newBookName = `book${numBooks}`;
+    
+    //create object with given information
+    const newBookName = new Book(title, pages, read);
+    //add new object to libraryArray
+    myLibrary.push(newBookName);
+
 }
+
+//create a button that once pressed, calls on the add-book function.
+let button1 = document.createElement('button');
+button1.innerHTML = 'Add a book';
+document.body.appendChild(button1);
+
+
