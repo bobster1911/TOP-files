6a09352ce5a0909e71c78d8232366a2a56d40cf2
Rob <47785553+bobster1911@users.noreply.github.com>
2022-02-16 16:00:03 -0800

    created button and added title and text

--- JS/ObjectPractice/library.js
+++ JS/ObjectPractice/library.js
@@ -8,25 +8,49 @@ function Book(title, pages, read) {
 
 function addBookToLibrary() {
     
+    //NOTE: change the prompt to a generated form field that looks nice.
+
     //ask for book information
     let title = prompt('Title of book: ');
     let pages = prompt('Number of pages: ');
     let read = prompt('Read or Unread: ');
-
-    //calculate how to name new addition
-    let numBooks = myLibrary.length;
-    let newBookName = `book${numBooks}`;
-    
+       
     //create object with given information
-    const newBookName = new Book(title, pages, read);
+    let book1 = new Book(title, pages, read);
     //add new object to libraryArray
-    myLibrary.push(newBookName);
+    myLibrary.push(book1);
 
 }
 
+function refreshBooks() {
+    //iterate through the array and create a div for each one.
+    let numBooks = myLibrary.length;
+    for (i = 0;i <= numBooks;i++) {
+        let ''div' + i' = document.createElement('div');
+        div[i].textContent = myLibrary[i];
+        document.body.appendChild(div[i]);
+    }
+}
+
+//website fluffies
+let title1 = document.createElement('H1');
+title1.textContent = 'Library App';
+document.body.appendChild(title1);
+
+let para1 = document.createElement('p');
+para1.textContent = 'Click the button below to add a book to the library.';
+document.body.appendChild(para1);
+
 //create a button that once pressed, calls on the add-book function.
 let button1 = document.createElement('button');
-button1.innerHTML = 'Add a book';
+button1.textContent = 'Add a book';
 document.body.appendChild(button1);
+button1.addEventListener("click", function() {addBookToLibrary();});
+
+
+
+
+
+
 
 
