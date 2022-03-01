39bef3ee9b8f7ec9f441c4252b52926b81b142a3
Rob <47785553+bobster1911@users.noreply.github.com>
2022-02-28 16:11:12 -0800

    started book object display on DOM

--- JS/ObjectPractice/library.js
+++ JS/ObjectPractice/library.js
@@ -14,30 +14,6 @@ function countBooks() {
 //iterate through the array and create a div for each one.
 let numBooks = (myLibrary.length - 1);
 
-
-
-
-//website fluffies
-let title1 = document.createElement('H1');
-title1.textContent = 'Library App';
-document.body.appendChild(title1);
-
-let para1 = document.createElement('p');
-para1.textContent = 'Click the button below to add a book to the library.';
-document.body.appendChild(para1);
-
-//create a button that once pressed, calls on the add-book function.
-let button1 = document.createElement('button');
-button1.textContent = 'Add a book';
-document.body.appendChild(button1);
-button1.addEventListener("click", function() {addBookToLibrary();});
-
-//refresh button to update the library contents
-let button2 = document.createElement('button');
-button2.textContent = 'Update';
-document.body.appendChild(button2);
-button2.addEventListener("click", function() {refreshBooks();});
-
 //FUNCTIONS
 
 function Book(title, author, pages, read) {
@@ -74,7 +50,8 @@ function refreshBooks() {
         
         bookDiv = document.createElement('div');
         bookDom[i] = bookDiv;
-        bookDiv.textContent = '\n' + myLibrary[i].title + '\n' + 
+        bookDiv.textContent = '\n' + myLibrary[i].title + '\n' + myLibrary[i].author 
+        + '\n' + myLibrary[i].readYet + '\n' + myLibrary[i].numberOfPages;
         document.body.appendChild(bookDiv);
     }
 }
@@ -89,7 +66,17 @@ let para1 = document.createElement('p');
 para1.textContent = 'Click the button below to add a book to the library.';
 document.body.appendChild(para1);
 
+//create a button that once pressed, calls on the add-book function.
+let button1 = document.createElement('button');
+button1.textContent = 'Add a book';
+document.body.appendChild(button1);
+button1.addEventListener("click", function() {addBookToLibrary();});
 
+//refresh button to update the library contents
+let button2 = document.createElement('button');
+button2.textContent = 'Update';
+document.body.appendChild(button2);
+button2.addEventListener("click", function() {refreshBooks();});
 
 
 
