6c97b84cd8c6d21875ef7539642e2b98d00b238b
c0de <robclarke93@yahoo.co.uk>
2022-04-18 05:09:09 -0700

    started module for game board generation

--- JS/ticTacToeGame/ticTacToe.js
+++ JS/ticTacToeGame/ticTacToe.js
@@ -1,7 +1,14 @@
-'use strict'
-
+// the logic behind the state of the game board.
 const gameBoard = (() => {
-    
+    const create = () => {
+        // create blank array
+        const LEN = 9;
+        const arr = new Array (LEN).fill(0);
+        return arr;
+    };
+    return {
+        create
+    };
 }) ();
 
 const displayBoard = (() => {
