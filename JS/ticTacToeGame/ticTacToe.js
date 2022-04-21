459516ba3ef0fcbfab209b362d6527e0e8043196
Rob <47785553+bobster1911@users.noreply.github.com>
2022-04-21 07:45:43 -0700

    building out functions for board logic

--- JS/ticTacToeGame/ticTacToe.js
+++ JS/ticTacToeGame/ticTacToe.js
@@ -1,14 +1,30 @@
 // the logic behind the state of the game board.
 const gameBoard = (() => {
+
     const create = () => {
         // create blank array
         const LEN = 9;
         const n = 0; // may have to be 'let', but not sure..
         const arr = new Array (LEN).fill(0);
-        //return arr;
-        const generate = () => {
-
+        //recursive function for creating the divs for the ticTacToe board.
+        const container = document.createElement('div');
+        container.setAttribute('id', 'boardContainer');
+        document.body.appendChild(container);
+        
+        const _genBoard = (n) => {
+            // base case
+            if (n == 0 || n < 0 || n > 8) {
+                return console.log('finished recursion for generating board divs.');
+            }
+            //recursion in replace of a for loop
+            const element = document.createElement('div');
+            element.addAttribute('id', `div${n}`);
+            // don't need to add to array, the numbers are arbitrarily mapped.
+            boardContainer.appendChild(element);
+            return _genBoard(n - 1);
+        };
     };
+
     return {
         create
     };
@@ -24,11 +40,3 @@ const displayBoard = (() => {
 const container = document.createElement('div');
 container.setAttribute('id', 'boardContainer');
 document.body.appendChild(container);
-
-// generate multiple divs within container
-const genBoard = (size, n) => {
-    
-    const element = document.createElement('div');
-    element.setAttribute('id', ('div' + `${n}`));
-    document.body.appendChild('')
-}
