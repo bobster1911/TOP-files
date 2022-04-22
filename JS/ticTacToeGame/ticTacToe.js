15ebc96973120f8556b6d22777b08b3ab0c0a836
Rob <47785553+bobster1911@users.noreply.github.com>
2022-04-22 15:57:31 -0700

    building out

--- JS/ticTacToeGame/ticTacToe.js
+++ JS/ticTacToeGame/ticTacToe.js
@@ -10,7 +10,7 @@ const gameBoard = (() => {
         const container = document.createElement('div');
         container.setAttribute('id', 'boardContainer');
         document.body.appendChild(container);
-        
+
         const _genBoard = (n) => {
             // base case
             if (n == 0 || n < 0 || n > 8) {
@@ -19,7 +19,7 @@ const gameBoard = (() => {
             //recursion in replace of a for loop
             const element = document.createElement('div');
             element.addAttribute('id', `div${n}`);
-            // don't need to add to array, the numbers are arbitrarily mapped.
+            // don't need to add to array, the numbers are arbitrarily mapped. Or I should say, the buttons will be hardcoded.
             boardContainer.appendChild(element);
             return _genBoard(n - 1);
         };
@@ -28,15 +28,50 @@ const gameBoard = (() => {
     return {
         create
     };
+
 }) ();
 
+// renders the gameboard output into GUI representation of the board
 const displayBoard = (() => {
     const create = () => {
-        
+        // CREATE CONTAINER AND INITIAL START BUTTON
+        const container = document.createElement('div');
+        container.setAttribute('id', 'btnContainer');
+        document.body.appendChild(container);
+
+        const button = document.createElement('button');
+        button.setAttribute('id', 'playBtn');
+        button.innerHTML = 'Start';
+        button.addEventListener('click', function () {
+            // function to remove some items and add new ones for player selection.
+                // 1 player vs. COM, 2 player
+            const button1 = document.createElement('button');
+            button1.setAttribute('id', '1 Player');
+            button1.innerHTML = 'Player 1';
+
+            const button2 = document.createElement('button');
+            button2.setAttribute('id', 'Player 2');
+            // carry on here ... <<<
+        });
+        container.appendChild(button);
+    }
+    return { 
+        create
     }
 }) ();
 
-// generate container
-const container = document.createElement('div');
-container.setAttribute('id', 'boardContainer');
-document.body.appendChild(container);
+// factory function for creating players
+const Player = (name, symbol, score) => {
+	const rng = () => {};
+	return {
+		name,
+		symbol,
+		score,
+		rng
+	};
+};
+
+// players could have properties for wins, losses and draws.
+	// this could then reset when 'play again' or such is clicked.
+
+displayBoard.create();
