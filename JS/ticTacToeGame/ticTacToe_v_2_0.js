0ed142eb596da78c2a767f7f75aba5b228800773
Rob Ubuntu <robclarke93@yahoo.co.uk>
2023-09-06 15:17:15 -0700

    working on player object assignment and organizing functions

--- JS/ticTacToeGame/ticTacToe_v_2_0.js
+++ JS/ticTacToeGame/ticTacToe_v_2_0.js
@@ -14,13 +14,12 @@
 // gameBoard - module
 // displayController - module
 
-const playerMaker = (name, symbol) => {
-    return { name, symbol };
-}
+
 
 // create players and assign symbols
-const player1 = playerMaker('Rob', 'X');
-const player2 = playerMaker('Glacier', 'O');
+// ***
+// const player1 = playerMaker('Rob', 'X');
+// const player2 = playerMaker('Glacier', 'O');
 
 // displayController module
 const displayController = ( () => {
@@ -104,23 +103,67 @@ const htmlModule = (() => {
         parentNode.insertBefore(element, reference);
     }
     // creates custom form
-    const makeForm = (location, idName, option1, option2) => {
-        htmlModule.create(location, 'form', id);
-        const setupForm = document.getElementById(id);
-        // input field and label
+    const makePlayerSelectForm = (location, formId, inputOneId, inputTwoId, inputType) => {
+        const formElement = document.createElement('form');
+        formElement.setAttribute('id', `${formId}`);
+        location.appendChild(formElement);
+
+        const formLabelOne = document.createElement('label');
+        formLabelOne.setAttribute('for', 'playerOneName');
+        formLabelOne.innerHTML = 'Player 1: '
+        const formInput1 = document.createElement('input');
+        formInput1.setAttribute('type', `${inputType}`);
+        formInput1.setAttribute('id', `${inputOneId}`);
+
+        const formLabelTwo = document.createElement('label');
+        formLabelTwo.setAttribute('for', 'playerTwoName');
+        formLabelTwo.innerHTML = 'Player 2: '
+        const formInput2 = document.createElement('input');
+        formInput2.setAttribute('type', `${inputType}`);
+        formInput2.setAttribute('id', `${inputTwoId}`);
+
+        const formSubmitBtn = document.createElement('button');
+        formSubmitBtn.setAttribute('id', 'formSubmitBtn');
+        formSubmitBtn.setAttribute('type', 'button'); // set button type to stop page refresh? look into this
+        formSubmitBtn.innerHTML = 'Ready!';
+        formSubmitBtn.addEventListener('click', function () {
+            playerNameAssign();
+            // create player objects using strings in form fields
+            // how to reference each player...
+        });
         
+        // trying out an iterative approach to appending all the elements for the form.
+        const formArray = [];
+        formArray.push(formLabelOne);
+        formArray.push(formInput1);
+        formArray.push(formLabelTwo);
+        formArray.push(formInput2);
+        formArray.push(formSubmitBtn);
+
+        formArray.forEach(item => formElement.appendChild(item));
+
+    };
+    // INTERNAL MODULE FUNCTIONS
+
+    // function for player name submit button
+    const playerNameAssign = () => {
+        const playerOneName = document.getElementById('p1Name').value;
+        const playerTwoName = document.getElementById('p2Name').value;
+        const player1 = playerMaker(playerOneName, 'X');
+        const player2 = playerMaker(playerTwoName, 'O');
     }
 
     return { 
         add,
         insert,
-        makeForm
+        makePlayerSelectForm
     }
 }) ();
 
 // NEXT: each time a new page setup is created, create a div container, so that removal only requires a single 
 //  div to be removed when possible.
 // --Currently creating the form for two player information.
+// -- hardcoding the symbol for each player but symbol selection could be a feature I add in the future.
 
 // THOUGHT:
 // for the array changing, I was thinking it would be a good idea to have a function that iterates over the array and 
@@ -134,6 +177,11 @@ const htmlModule = (() => {
 // Generate front page for Tic Tac Toe game
 
 // NOTE: I think I'm going to keep all of the processes in functions
+
+
+const playerMaker = (name, symbol) => {
+    return { name, symbol };
+}
 
 const playTicTacToe = () => { // [1]
 
@@ -152,12 +200,13 @@ const playTicTacToe = () => { // [1]
 
     const playerSelectPage = (rmDiv) => {
         // remove start button
-        startPageDiv.remove();
+        rmDiv.remove();
         // recreate div
-
+        htmlModule.add(document.body, 'div', 'playerSelectDiv', '');
+        const playerSelectDiv = document.getElementById('playerSelectDiv');
         // add player options to new div
-        htmlModule.add(, 'button', 'onePlayerBtn', '1 Player');
-        htmlModule.add(document.body, 'button', 'twoPlayerBtn', '2 Player');
+        htmlModule.add(playerSelectDiv, 'button', 'onePlayerBtn', '1 Player');
+        htmlModule.add(playerSelectDiv, 'button', 'twoPlayerBtn', '2 Player');
 
         const onePlayerBtn = document.getElementById('onePlayerBtn');
         onePlayerBtn.onclick = function() {
@@ -165,7 +214,7 @@ const playTicTacToe = () => { // [1]
         };
         const twoPlayerBtn = document.getElementById('twoPlayerBtn');
         twoPlayerBtn.onclick = function() {
-            twoPlayerGame();
+            twoPlayerGame(playerSelectDiv);
         };
 
     }
@@ -174,21 +223,25 @@ const playTicTacToe = () => { // [1]
         console.log('one player game selected');
     }
 
-    const twoPlayerGame = () => {
+    const twoPlayerGame = (rmDiv) => {
         console.log('two player game selected');
+        rmDiv.remove();
+        // gather names of player 1 and 2
+        htmlModule.add(document.body, 'div', 'playerEntryDiv', '');
+        const playerEntryDiv = document.getElementById('playerEntryDiv');
+        htmlModule.makePlayerSelectForm(playerEntryDiv, 'playerSelectForm', 'p1Name', 'p2Name', 'text');
     }
 
-    const twoPlayerForm = () => {
-        // remove buttons
-        // create form
 
-    }
 
     startPage();
 }; // [1]
 
 playTicTacToe();
 
-
+// Features to add:
+// -- symbol select at player select screen
+// -- single player game against the AI
+// -- Themes for the game; this would be a good way to practice my CSS styling 
 
 
